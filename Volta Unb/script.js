b2dc76e0869f5c3b3e5a3d1a9665993a31c19f78
Jeovannny <165398171+Jeovannny@users.noreply.github.com>
2025-12-19 17:42:38 -0300

    Delete Volta Unb directory

--- Volta Unb/script.js
+++
@@ -1,5 +0,0 @@
-//Calculando a diferença entre dias
-const hoje = moment();
-const voltaAulas = moment("2026-03-16");
-const diferencaDias = voltaAulas.diff(hoje, 'days');
-document.getElementById("dias").innerHTML = diferencaDias;
