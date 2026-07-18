548503c935783e105d657cdd2328bd69ad9b52c3
Jeovannny <165398171+Jeovannny@users.noreply.github.com>
2026-07-18 18:13:00 -0300

    Update return date for school resumption calculation

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //Calculando a diferença entre dias
 const hoje = moment();
-const voltaAulas = moment("2026-03-16");
+const voltaAulas = moment("2026-08-10");
 const diferencaDias = voltaAulas.diff(hoje, 'days');
 document.getElementById("dias").innerHTML = diferencaDias;
